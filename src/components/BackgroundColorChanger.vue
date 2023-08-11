6edc61a4377c30c1f7c40aeeb95508640971fa9e
Arjansho1~ <arjansho@amazon.de>
2023-08-11 17:36:42 +0200

    Commit

---
+++ src/components/BackgroundColorChanger.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div :class="[backgroundColor]">
+        <button @click="changeColor">Change Color</button>
+        <h1>This is a box with a dynamic background color!</h1>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+     data() {
+        return {
+            backgroundColor: ref('red')
+        }
+     },
+    methods: {
+        changeColor (){
+            this.backgroundColor = this.backgroundColor === 'red' ? 'blue' : 'red';
+        }
+    },
+}
+</script>
+
+<style lang="css">
+   .red {
+    background-color: red
+   }
+   .blue {
+    background-color: blue
+   }
+</style>
